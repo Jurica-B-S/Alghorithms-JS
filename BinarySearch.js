e50dcecb2bc94079306beaf063de46f064e2be78
Jurica Brekalo Strbic <Jurica.Brekalo.Strbic@gmail.com>
2019-04-15 14:51:08 +0200

    Create BinarySearch.js

---
+++ BinarySearch.js
@@ -0,0 +1,14 @@
+'use strict';
+
+//use for array of numbers
+
+function binarySearch(array){
+  let high = array.length - 1;
+  let low = 0;
+  
+  if(!Array.isArray(array)){
+  trow("Wrong file type - send an array of numbers")
+  }
+  
+  
+}
