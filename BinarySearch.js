4f7376d3f4ed14918af33153a3f46363b8682685
Jurica Brekalo Strbic <Jurica.Brekalo.Strbic@gmail.com>
2019-04-16 10:44:38 +0200

    Update BinarySearch.js

--- BinarySearch.js
+++ BinarySearch.js
@@ -2,13 +2,32 @@
 
 //use for array of numbers
 
-function binarySearch(array){
+function binarySearch(array, number){
   let high = array.length - 1;
   let low = 0;
-  
+
   if(!Array.isArray(array)){
-  trow("Wrong file type - send an array of numbers")
+  throw("Wrong file type - send an array of numbers")
   }
   
-  
+  let mid = low +  Math.floor((high - low) / 2);
+
+  while(mid >= low){
+    if(array[mid] === number){
+      return true;
+    }
+    if(number < array[mid]){
+      high  = mid-1;
+      mid = Math.floor((low + high)/2);
+
+
+    }
+    else {
+      low  = mid + 1;
+      mid = Math.floor((low + high)/2);
+    
+    }
+    
+  }
+  return false; 
 }
